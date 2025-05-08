4d95649ac51ddde00ddbc4db9f5ff6eb07d0ed2b
estudiante <estudiante@10.220.4.135>
2025-05-08 17:30:54 -0500

    COMMIT 2 - Finished methods "nuevoUsuario" and "verUsuario"

--- src/bookworm/BookWorm.java
+++ src/bookworm/BookWorm.java
@@ -9,10 +9,6 @@ public class BookWorm {
     private static ArrayList<Usuario> usuarios = new ArrayList();
     
     
-    
-    
-    
-    
     public static void main(String[] args) {
         //libro y usuario preexistente
         libros.add(new Libro("Terraria 2", "Historia"));
@@ -23,23 +19,24 @@ public class BookWorm {
             System.out.println("");
             System.out.println("----------------------------------------------------");
             System.out.println("Bienvenido al software de administrador de Bookworm");
-            System.out.println("Para registrar un usuario nuevo ingrese 1");        
-            System.out.println("Para ingresar un libro nuevo ingrese 2");
-            System.out.println("Ver los libros ingrese 3");
-            System.out.println("Para salir ingrese otro número distinto");
+            System.out.println("Para registrar un usuario nuevo ingrese 1");     
+            System.out.println("Para ver los usuarios registrados ingrese 2");
+            System.out.println("Para ingresar un libro nuevo ingrese 3");
+            System.out.println("Para ver los libros ingrese 4");
+            System.out.println("Para salir ingrese otro numero distinto");
             System.out.println(" ");
             switch(tryInteger(scanner)){
             case 1:
-                nuevoLibro(scanner, libros);
+                nuevoUsuario(scanner);
                 break;
             case 2:
-                System.out.println("Ingreso 2");
+                verUsuarios();
                 break;
             case 3:
-                verLibros(libros);
+                nuevoLibro(scanner);
                 break;
             case 4:
-                System.out.println("Ingreso 4");
+                verLibros();
                 break;
             default:
                 System.out.println(" ");
@@ -70,7 +67,53 @@ public class BookWorm {
         }    
     }    
     
-    public static void nuevoLibro(Scanner scanner, ArrayList<Libro>  libros){
+    
+    public static void nuevoUsuario(Scanner scanner){
+        String nombre, cedula, genero;
+        System.out.println(" ");
+        System.out.println("Selecciono ingreso de nuevo usuario");
+        System.out.println("Ingrese el nombre:");
+        nombre = scanner.nextLine();
+        System.out.println("Ingrese el número de cedula");
+        cedula = Integer.toString(tryInteger(scanner));
+        System.out.println("Para elegir el genero favorito del usuario ingrese el número correspondiente(solo puede elegir uno)");
+        System.out.println("1: Ciencia");
+        System.out.println("2: Literatura");
+        System.out.println("3: Ingenieria");
+        System.out.println("4: Matematicas");
+        System.out.println("otro número: Historia");
+        System.out.println(" ");
+        switch(tryInteger(scanner)){
+            case 1:
+                genero="Ciencia";
+                break;
+            case 2:
+                genero="Literatura";
+                break;
+            case 3:
+                genero="Ingenieria";
+                break;
+            case 4:
+                genero="matematicas";
+                break;
+            default:
+                genero="historia";
+                break;
+        }
+        usuarios.add(new Usuario(nombre, cedula, genero));
+    }
+    public static void verUsuarios(){
+        System.out.println("Usuarios registrados");
+        System.out.println(" ");
+        for(int i=0; i<usuarios.size();i++){
+            System.out.println((i+1)+". Nombre: "+usuarios.get(i).getNombre());
+            System.out.println("  No. de cedula: "+usuarios.get(i).getCedula());
+            System.out.println("  Genero: "+usuarios.get(i).getGenero());         
+        }     
+    }
+    
+    
+    public static void nuevoLibro(Scanner scanner){
         String nombre, genero;
         System.out.println(" ");
         System.out.println("Selecciono ingreso de nuevo libro");
@@ -102,59 +145,16 @@ public class BookWorm {
         }
         libros.add(new Libro(nombre, genero));
     }
-    
-    public static void verLibros(ArrayList<Libro> libros){
+    public static void verLibros(){
         System.out.println("Libros disponibles: ");
+        System.out.println(" ");
         for(int i=0; i<libros.size();i++){
             System.out.println((i+1)+". Nombre: "+libros.get(i).getNombre());
-            System.out.println("  Genero: "+libros.get(i).getGenero());
-            
-        }    
-        
+            System.out.println("  Genero: "+libros.get(i).getGenero());  
+        }      
     }
     
-    public static void nuevoUsuario(Scanner scanner, ArrayList<Libro>  libros){
-        String nombre, genero;
-        System.out.println(" ");
-        System.out.println("Selecciono ingreso de nuevo libro");
-        System.out.println("Ingrese el nombre:");
-        nombre = scanner.nextLine();
-        System.out.println("Para elegir el genero ingrese el número correspondiente(solo puede elegir uno)");
-        System.out.println("1: Ciencia");
-        System.out.println("2: Literatura");
-        System.out.println("3: Ingenieria");
-        System.out.println("4: Matematicas");
-        System.out.println("otro número: Historia");
-        System.out.println(" ");
-        switch(tryInteger(scanner)){
-            case 1:
-                genero="Ciencia";
-                break;
-            case 2:
-                genero="Literatura";
-                break;
-            case 3:
-                genero="Ingenieria";
-                break;
-            case 4:
-                genero="matematicas";
-                break;
-            default:
-                genero="historia";
-                break;
-        }
-        libros.add(new Libro(nombre, genero));
-    }
     
-    public static void verUsuarios(ArrayList<Libro> libros){
-        System.out.println("Libros disponibles: ");
-        for(int i=0; i<libros.size();i++){
-            System.out.println((i+1)+". Nombre: "+libros.get(i).getNombre());
-            System.out.println("  Genero: "+libros.get(i).getGenero());
-            
-        }    
-        
-    }
     
     
     
